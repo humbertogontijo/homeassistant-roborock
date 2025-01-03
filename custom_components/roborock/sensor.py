ee669383cc7140a4c76277146b10c323a7ec9fc9
Dante F <dante.fragapane@gmail.com>
2025-01-03 09:30:10 -0500

    Changes to fix issue #673, AREA_SQUARE_METERS deprecated

--- custom_components/roborock/sensor.py
+++ custom_components/roborock/sensor.py
@@ -16,7 +16,7 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import AREA_SQUARE_METERS, PERCENTAGE, UnitOfTime
+from homeassistant.const import UnitOfArea.SQUARE_METERS, PERCENTAGE, UnitOfTime
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -100,7 +100,7 @@ VACUUM_SENSORS = {
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     f"last_clean_{ATTR_LAST_CLEAN_AREA}": RoborockSensorDescription(
-        native_unit_of_measurement=AREA_SQUARE_METERS,
+        native_unit_of_measurement=UnitOfArea.SQUARE_METERS,
         key="area",
         value=lambda value, _: round(value / 1000000, 1),
         icon="mdi:texture-box",
